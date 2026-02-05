80e10a90e614ac76876016060066a4f769076a59
7aamin Damy <marvmustaf@gmail.com>
2026-02-05 21:06:00 +0000

    aun no aparecen imagenes..

--- next.config.ts
+++ next.config.ts
@@ -14,43 +14,6 @@ const nextConfig: NextConfig = {
 
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'roofmaster.ca',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.wixstatic.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'westpacroofing.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'phs-roof.com',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
